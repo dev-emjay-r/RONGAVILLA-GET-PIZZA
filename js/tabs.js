1995ca142e35e512b4961edbbfd59aad92ba83d7
dev-emjay-r <dev.emjayrongavilla@gmail.com>
2026-03-05 14:05:25 +0800

    Refactor tab functionality in tabs.js for improved modularity and add menu section with dynamic tab content in menu.html

--- js/tabs.js
+++ js/tabs.js
@@ -2,34 +2,41 @@ const initTabs = () =>{
     const tabs = document.querySelectorAll(".tabIcons ul li");
     const tabContents = document.querySelectorAll(".tabItem");
 
-    tabs.forEach((tab)=> {
-      tab.addEventListener("click",() => {
-        //Clear Active Tabs
-        clearActiveTabs();
-        clearActiveContents();
-        console.log("hello");
-        // select tab content
-        const tabID = tab.id;
-        console.log(tabID);
-        const detail = document.querySelector(`#${tabID}-card`);
+    const tabCards = 
+  document.querySelectorAll(".tabs-menu ul li");
 
-        //set class to active
-        tab.classList.add("active");
-        if (detail) {
-          detail.classList.add("active");
-        }
-      })
-    });
 
-    function clearActiveTabs() {
+    addClickActiveFunction(tabs, tabContents);
+    addClickActiveFunction(tabCards, tabContents);
+    // dependency injection
+    function clearActiveTabs(tabs) {
       tabs.forEach((tab) => {
         tab.classList.remove("active");
       });
     }
-    function clearActiveContents() {
+    function clearActiveContents(tabContents) {
       tabContents.forEach((contents) => {
         contents.classList.remove("active");
       });
+    }
+    function addClickActiveFunction(tabs, tabContents) {
+       tabs.forEach((tab) => {
+         tab.addEventListener("click", () => {
+           //Clear Active Tabs
+           clearActiveTabs(tabs);
+           clearActiveContents(tabContents);
+           // select tab content
+           const tabID = tab.id;
+           console.log(tabID);
+           const detail = document.querySelector(`#${tabID}-card`);
+
+           //set class to active
+           tab.classList.add("active");
+           if (detail) {
+             detail.classList.add("active");
+           }
+         });
+       });
     }
 }
 
