280071db067d4ee8436f8f87d148256b83cf2039
dev-emjay-r <dev.emjayrongavilla@gmail.com>
2026-03-05 10:42:36 +0800

    added sliders and other fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import tabs from './tabs.js';
 import initToggle from './toggle-menu.js';
 import initAccordion from './accordion.js';
+import initSlider from './slider.js';
 tabs();
 initToggle();
 initAccordion();
+initSlider();
---
+++ js/slider.js
@@ -0,0 +1,17 @@
+function initSlider() {
+    var clientSlider = tns({
+      container: ".clientSlider",
+      items: 1,
+      autoplay: true,
+      controls: false,
+      autoplayButtonOutput: false,
+      nav: false,
+      speed:100
+    });
+
+
+}
+
+
+
+export default initSlider;
